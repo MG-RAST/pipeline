7fbf9374531713aa261741491d82730a5faf71e9
Travis Harrison <tharriso@dieppe-1.mcs.anl.gov>
2012-09-12 11:47:06 -0500

    added option to stitch togther singlet pairs, improved fastq parsing speed

--- bin/pairend_join.py
+++ bin/pairend_join.py
@@ -2,7 +2,7 @@
 
 import sys, os, shlex, string, random, subprocess
 from optparse import OptionParser
-from Bio import SeqIO
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
 __doc__ = """
 Join paired-end fastq files.  Unjoined IDs uniquified.
@@ -23,16 +23,36 @@ def random_str(size=6):
 
 def append_id(fname, text):
     tmphdl = open(fname+'.tmp', 'w')
-    for rec in SeqIO.parse(fname, 'fastq'):
-        rec.id = rec.id+'.'+text
-        tmphdl.write(rec.format('fastq'))
+    for head, seq, qual in FastqGeneralIterator(open(fname)):
+        tmphdl.write("@%s.%s\n%s\n+\n%s\n" %(head.split()[0], text, seq, qual))
     tmphdl.close()
     os.rename(fname+'.tmp', fname)
+
+def stitch_seqs(outfile, file1, file2, blen):
+    bseq  = 'N' * blen
+    bqual = '!' * blen
+    itr1 = FastqGeneralIterator(open(file1))
+    itr2 = FastqGeneralIterator(open(file2))
+    rec1 = itr1.next()
+    rec2 = itr2.next()
+    outh = open(outfile, 'w')
+    while 1:
+        outh.write("@%s\n%s%s%s\n+\n%s%s%s\n" %(random_str(8), rec1[1], bseq, rec2[1], rec1[2], bqual, rec2[2]))
+        try:
+            rec1 = itr1.next()
+            rec2 = itr2.next()
+        except (StopIteration, IOError):
+            break
+    outh.close()
 
 def main(args):
     usage  = "usage: %prog -o <output file> <read 1 file> <read 2 file>"+__doc__
     parser = OptionParser(usage)
     parser.add_option("-o", "--output", dest="output", default=None, help="Output joined fastq file.")
+    parser.add_option("-m", "--min_overlap", dest="min_overlap", type="int", default=8, help="N-minimum overlap [default 8]")
+    parser.add_option("-p", "--per_diff", dest="per_diff", type="int", default=10, help="N-percent maximum difference [default 10 %]")
+    parser.add_option("-s", "--stitch", dest="stitch", action="store_true", default=False, help="stitch together singlet pairs, default to add seperatly.")
+    parser.add_option("-n", "--n_num", dest="n_num", type="int", default=10, help="Number of Ns to stitch singlets together with [default 10]")
     parser.add_option("-t", "--tmp_dir", dest="tmpdir", default="/tmp", help="DIR for intermediate files (must be full path), deleted at end [default '/tmp']")
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Wordy [default off]")
 
@@ -45,14 +65,20 @@ def main(args):
 
     if opts.verbose: sys.stdout.write("Joining %s and %s ... "%(input1, input2))
     tmp_prefix = os.path.join(opts.tmpdir, random_str())
-    sout, serr = run_cmd(['fastq-join', '-m', '10', input1, input2, '-o', tmp_prefix+'.u1', '-o', tmp_prefix+'.u2', '-o', tmp_prefix+'.join'], None)
+    per_string = "%.2f"%(opts.per_diff/100.0)
+    sout, serr = run_cmd(['fastq-join', '-m', str(opts.min_overlap), '-p', per_string, input1, input2, '-o', tmp_prefix+'.u1', '-o', tmp_prefix+'.u2', '-o', tmp_prefix+'.join'], None)
     if opts.verbose: sys.stdout.write("Done\n%s"%sout)
 
     if opts.verbose: sys.stdout.write("Merging joined and un-joined files ... ")
-    append_id(tmp_prefix+'.u1', '1')
-    append_id(tmp_prefix+'.u2', '2')
-    out_hdl  = open(opts.output, 'w')
-    _so, _se = run_cmd(['cat', tmp_prefix+'.u1', tmp_prefix+'.u2', tmp_prefix+'.join'], out_hdl)
+    out_hdl = open(opts.output, 'w')
+    if opts.stitch:
+        stitch_seqs(tmp_prefix+'.stitch', tmp_prefix+'.u1', tmp_prefix+'.u2', opts.n_num)
+        _so, _se = run_cmd(['cat', tmp_prefix+'.join', tmp_prefix+'.stitch'], out_hdl)
+        os.remove(tmp_prefix+'.stitch')
+    else:
+        append_id(tmp_prefix+'.u1', '1')
+        append_id(tmp_prefix+'.u2', '2')
+        _so, _se = run_cmd(['cat', tmp_prefix+'.join', tmp_prefix+'.u1', tmp_prefix+'.u2'], out_hdl)
     out_hdl.close()
     os.remove(tmp_prefix+'.u1')
     os.remove(tmp_prefix+'.u2')
