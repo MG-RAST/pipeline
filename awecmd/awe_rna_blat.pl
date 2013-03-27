f1a3407f4b7b98bceb62dd0746d3fd281b2f8a00
Wei Tang <wtang222@gmail.com>
2013-03-27 13:05:12 -0500

    add awe_rna_blat.pl, awe wrapper for mg-rast stage 440

--- awecmd/awe_rna_blat.pl
+++ awecmd/awe_rna_blat.pl
@@ -11,7 +11,7 @@ umask 000;
 
 my $stage_name="search";
 my $stage;
-my $stage_id = "425";
+my $stage_id = "440";
 my $runcmd   = "parallel_search.py";
 
 # options
@@ -21,14 +21,11 @@ my $rna_nr  = "md5nr.clust";
 my $proc    = 8;
 my $size    = 100;
 my $ident   = 70;
-my $output  = $stage_id.".rna.fna";
+my $output  = $stage_id.".rna.sim";
 my $options = GetOptions ("job=i"    => \$job_num,
 			  "input=s"  => \$fasta,
                           "output:s" => \$output,
 			  "rna_nr=s" => \$rna_nr,
-			  "proc:i"   => \$proc,
-			  "size:i"   => \$size,
-			  "ident:i"  => \$ident,
 			 );
 
 my $refdb_dir = ".";
@@ -52,10 +49,11 @@ unless (-s $fasta) {
 print "rna_nr_path=".$rna_nr_path."\n"; 
 
 my $run_dir = getcwd();
-system("$runcmd -v -p $proc -s $size -i 0.$ident -d $run_dir $rna_nr_path $fasta $run_dir/$output >> $run_dir/$runcmd.out 2>&1") == 0 or exit __LINE__;
+
+system("blat -out=blast8 -t=dna -q=dna -fastMap $rna_nr_path $fasta stdout | bleachsims -s - -o $output -r 0 > $run_dir/blat.out 2>&1") == 0 or exit __LINE__;
 
 exit(0);
 
 sub print_usage{
-    print "USAGE: awe_rna_search.pl -input=<input fasta> [-rna_nr=<rna_nr_file, default: md5nr.clust> -proc=<number of threads, default: 8 > -size=<size, default: 100> -output=<output fasta default:425.rna.fna> -ident=<ident percentage, default: 70>] \n";
+    print "USAGE: awe_rna_blat.pl -input=<input fasta> [-rna_nr=<rna_nr_file, default: md5nr.clust> -output=<output fasta default:440.rna.sim>] \n";
 }
