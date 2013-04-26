42acc88d994140ff93c3ce739e8340794b808c59
Jared Bischof <jbischof@gmail.com>
2013-04-26 15:13:11 -0500

    Removing changes

--- lib/Pipeline.pm
+++ lib/Pipeline.pm
@@ -122,13 +122,9 @@ sub get_job_owner {
 }
 
 sub get_job_options {
-  if($Pipeline_Conf::jobcache_db_avail) {
-    my $job = shift;
-    my $job_obj = get_jobcache_info($job);
-    return $job_obj->{options} ? $job_obj->{options} : "";
-  } else {
-    return "";
-  }
+  my $job = shift;
+  my $job_obj = get_jobcache_info($job);
+  return $job_obj->{options} ? $job_obj->{options} : "";
 }
 
 sub get_jobcache_info { 
