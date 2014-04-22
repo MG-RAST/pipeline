f6db3feaa346308f2035387089a0f3face006ac4
Travis Harrison <guumby@gmail.com>
2014-04-22 12:46:39 -0500

    updated / cleaned script

--- awecmd/awe_genecalling.pl
+++ awecmd/awe_genecalling.pl
@@ -11,19 +11,19 @@ use Cwd;
 umask 000;
 
 # options
+my $out_prefix = "genecall";
 my $fasta   = "";
-my $output  = "";
 my $proc    = 8;
 my $size    = 100;
 my $type    = "454";
 my $help    = 0;
 my $options = GetOptions (
-        "input=s"  => \$fasta,
-        "output=s" => \$output,
-		"proc:i"   => \$proc,
-		"size:i"   => \$size,
-		"type:s"   => \$type,
-        "help!"    => \$help
+        "out_prefix=s" => \$out_prefix,
+        "input=s" => \$fasta,
+		"proc:i"  => \$proc,
+		"size:i"  => \$size,
+		"type:s"  => \$type,
+        "help!"   => \$help
 );
 
 if ($help){
@@ -31,10 +31,6 @@ if ($help){
     exit 0;
 }elsif (length($fasta)==0){
     print "ERROR: An input file was not specified.\n";
-    print_usage();
-    exit __LINE__;
-}elsif (length($output)==0){
-    print "ERROR: An output file was not specified.\n";
     print_usage();
     exit __LINE__;
 }elsif (! -e $fasta){
@@ -51,22 +47,11 @@ unless (exists $types{$type}) {
 }
 
 my $run_dir = getcwd;
-print "parallel_FragGeneScan.py -v -p $proc -s $size -t $types{$type} -d $run_dir $fasta $output\n";
-PipelineAWE::run_cmd("parallel_FragGeneScan.py -v -p $proc -s $size -t $types{$type} -d $run_dir $fasta $output");
-
-
-system("parallel_FragGeneScan.py -v -p $proc -s $size -t $types{$type} -d $run_dir $input_fasta $out_prefix >> $runcmd.out 2>&1") == 0 or exit __LINE__;
-
-system("seqUtil --stdfasta -i $out_prefix.faa -o $out_prefix.clean.faa") ==0 or system("mv $out_prefix.faa $out_prefix.clean.faa");
-
-if (length($final_output) == 0) {
-  $final_output = $out_prefix.".faa";
-} 
-system("mv $out_prefix.clean.faa $final_output") == 0 or exit __LINE__;
-system("mv $out_prefix.ffn $out_prefix.fna")==0 or system("touch $out_prefix.fna");
+print "parallel_FragGeneScan.py -v -p $proc -s $size -t $types{$type} -d $run_dir $fasta $out_prefix\n";
+PipelineAWE::run_cmd("parallel_FragGeneScan.py -v -p $proc -s $size -t $types{$type} -d $run_dir $fasta $out_prefix");
 
 exit(0);
 
 sub print_usage{
-    print "USAGE: awe_genecalling.pl -input=<input fasta> -output=<output faa> [-type=<454 | sanger | illumina | complete> -proc=<number of threads, default: 8> -size=<size, default: 100>]\n";
+    print "USAGE: awe_genecalling.pl -input=<input fasta> [-out_prefix=<output prefix> -type=<454 | sanger | illumina | complete> -proc=<number of threads, default: 8> -size=<size, default: 100>]\n";
 }
