b4a7ebb0910eff04535b384a8a2f8938a6096465
Travis Harrison <guumby@gmail.com>
2014-04-22 14:59:56 -0500

    cluster with cd-hit

---
+++ awecmd/awe_cluster.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/env perl
+
+#input: fasta
+#outputs:  ${out_prefix}.aa${pid}.faa, ${out_prefix}.aa${pid}.mapping
+#            OR
+#          ${out_prefix}.rna${pid}.fna or ${out_prefix}.rna${pid}.mapping
+#            OR
+#          ${out_prefix}.dna${pid}.fna or ${out_prefix}.dna${pid}.mapping
+
+use strict;
+use warnings;
+no warnings('once');
+
+use PipelineAWE;
+use Getopt::Long;
+use Cwd;
+umask 000;
+
+# options
+my $out_prefix = "cluster";
+my $fasta   = "";
+my $pid     = 90;
+my $mem     = 16;
+my $aa      = 0;
+my $rna     = 0;
+my $dna     = 0;
+my $help    = 0;
+my $options = GetOptions (
+        "out_prefix=s" => \$out_prefix,
+        "input=s" => \$fasta,
+		"pid=i"   => \$pid,
+		"mem=i"   => \$mem,
+		"aa!"     => \$aa,
+        "rna!"    => \$rna,
+        "dna!"    => \$dna,
+        "help!"   => \$help
+);
+
+if ($help){
+    print_usage();
+    exit 0;
+}elsif (length($fasta)==0){
+    print "ERROR: An input file was not specified.\n";
+    print_usage();
+    exit __LINE__;
+}elsif (! -e $fasta){
+    print "ERROR: The input sequence file [$fasta] does not exist.\n";
+    print_usage();
+    exit __LINE__;
+}elsif ((! $pid) || ($pid < 40)){
+    print "ERROR: The percent identity must be greater than 50\n";
+    print_usage();
+    exit __LINE__;
+}
+
+my ($cmd, $word, $output);
+if ($aa) {
+    ($cmd, $word, $output) = ("cd-hit", word_length("aa", $pid), $out_prefix.".aa".$pid."faa");
+} elsif ($rna) {
+    ($cmd, $word, $output) = ("cd-hit-est", word_length("rna", $pid), $out_prefix.".rna".$pid."fna");
+} elsif ($dna) {
+    ($cmd, $word, $output) = ("cd-hit-est", word_length("dna", $pid), $out_prefix.".dna".$pid."fna");
+}
+my $memory = $mem * 1024;
+
+print "$cmd -n $word -d 0 -T 0 -M $memory -c 0.$pid -i $fasta -o $output\n";
+#PipelineAWE::run_cmd("$cmd -n $word -d 0 -T 0 -M $memory -c 0.$pid -i $fasta -o $output");
+
+exit(0);
+
+sub print_usage {
+    print "USAGE: awe_cluster.pl -input=<input fasta> <-aa|-rna> -pid=<percentage of identification, default 90> [-out_prefix=<output prefix> -mem=<memory usage in GB, default is 16>]\n";
+}
+
+# determine optimal word length for clustering
+sub word_length {
+    my ($type, $pid) = @_;
+    
+    if ($type eq 'aa') {
+        if ($pid > 70) {
+            return 5;
+        } elsif ($pid > 60) {
+            return 4;
+        } elsif ($pid > 50) {
+            return 3;
+        } else {
+            return 2;
+        }
+    } else {
+        if ($pid > 98) {
+            return 10;
+        } elsif ($pid > 95) {
+            return 9;
+        } elsif ($pid > 90) {
+            return 8;
+        } elsif ($pid > 88) {
+            return 7;
+        } elsif ($pid > 85) {
+            return 6;
+        } elsif ($pid > 80) {
+            return 5;
+        } elsif ($pid > 75) {
+            return 4;
+        } elsif ($pid > 60) {
+            return 3;
+        } else {
+            return 2;
+        }
+    }
+}
+
