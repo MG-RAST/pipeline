89998ae53545dc47c1e3ba9052639a59d6ed846a
Travis Harrison <guumby@gmail.com>
2014-04-24 13:29:25 -0500

    rename script

--- awecmd/awe_annotate_sims.pl
+++ awecmd/awe_annotate_sims.pl
@@ -15,7 +15,7 @@ use Getopt::Long;
 umask 000;
 
 # options
-my $out_prefix = "process_sims";
+my $out_prefix = "annotate_sims";
 my $input   = "";
 my $memhost = "localhost:11211";
 my $memkey  = '_ach';
@@ -63,7 +63,7 @@ PipelineAWE::run_cmd($cmd);
 exit(0);
 
 sub get_usage {
-    return "USAGE: awe_process_sims.pl -input=<input sims> <-aa|-rna> [-output_prefix=<output prefix> -mem_host=<memcache host> -mem_key=<memcache key>]\n".
+    return "USAGE: awe_annotate_sims.pl -input=<input sims> <-aa|-rna> [-output_prefix=<output prefix> -mem_host=<memcache host> -mem_key=<memcache key>]\n".
            "outputs: \${out_prefix}.aa.sims.filter, \${out_prefix}.aa.expand.protein, \${out_prefix}.aa.expand.lca, \${out_prefix}.aa.expand.ontology\n".
            "           OR\n".
            "         \${out_prefix}.rna.sims.filter, \${out_prefix}.rna.expand.rna, \${out_prefix}.rna.expand.lca\n";
