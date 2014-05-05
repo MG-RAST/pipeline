73e5e5c5c4a31632f0118066eb1829b11af71a58
Travis Harrison <guumby@gmail.com>
2014-05-05 10:28:39 -0500

    fix usage string

--- awecmd/awe_annotate_sims.pl
+++ awecmd/awe_annotate_sims.pl
@@ -63,7 +63,7 @@ PipelineAWE::run_cmd($cmd);
 exit(0);
 
 sub get_usage {
-    return "USAGE: awe_annotate_sims.pl -input=<input sims> <-aa|-rna> [-output_prefix=<output prefix> -mem_host=<memcache host> -mem_key=<memcache key>]\n".
+    return "USAGE: awe_annotate_sims.pl -input=<input sims> <-aa|-rna> [-out_prefix=<output prefix> -mem_host=<memcache host> -mem_key=<memcache key>]\n".
            "outputs: \${out_prefix}.aa.sims.filter, \${out_prefix}.aa.expand.protein, \${out_prefix}.aa.expand.lca, \${out_prefix}.aa.expand.ontology\n".
            "           OR\n".
            "         \${out_prefix}.rna.sims.filter, \${out_prefix}.rna.expand.rna, \${out_prefix}.rna.expand.lca\n";
