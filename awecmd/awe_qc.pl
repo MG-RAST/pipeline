48a7d0e6e5f64d584e7552f7db4b60b91a4af04c
Travis Harrison <guumby@gmail.com>
2014-05-06 16:30:27 -0500

    qc outputs a single stats file in json format

--- awecmd/awe_qc.pl
+++ awecmd/awe_qc.pl
@@ -3,14 +3,17 @@
 #input: .fna or .fastq
 #outputs: ${out_prefix}.drisee.stats, ${out_prefix}.drisee.info,
 #         ${out_prefix}.consensus.stats, ${out_prefix}.kmer.$len.stats,
-#         ${out_prefix}.assembly.coverage, ${out_prefix}.assembly.coverage.stats
+#         ${out_prefix}.assembly.coverage
+#         ${out_prefix}.qc.stats
 
 use strict;
 use warnings;
 no warnings('once');
 
+use JSON;
 use PipelineAWE;
 use List::Util qw(first max min sum);
+use POSIX qw(strftime floor);
 use Getopt::Long;
 use Cwd;
 umask 000;
@@ -55,11 +58,11 @@ if ($help){
 my @kmers = split(/,/, $kmers);
 my $bad_kmer = 0;
 foreach (@kmers) {
-  if ($_ !~ /^\d+$/) { $bad_kmer = 1; }
+    if ($_ !~ /^\d+$/) { $bad_kmer = 1; }
 }
 if ((@kmers == 0) || $bad_kmer) {
-  print STDERR "ERROR: invalid kmeer list: $kmers.\n";
-  exit(1);
+    print STDERR "ERROR: invalid kmeer list: $kmers.\n";
+    exit(1);
 }
 
 my $format = ($infile =~ /\.(fq|fastq)$/) ? 'fastq' : 'fasta';
@@ -72,74 +75,191 @@ for my $ov (split ":", $filter_options) {
     }
 }
 
+my $output  = $out_prefix.".qc.stats";
 my $d_stats = $out_prefix.".drisee.stats";
 my $d_info  = $out_prefix.".drisee.info";
 my $c_stats = $out_prefix.".consensus.stats";
 my $a_file  = $out_prefix.".assembly.coverage";
+my $qc_stat = {};
 my $run_dir = getcwd;
 
 if ($assembled != 1) {
-  # create drisee table
-  PipelineAWE::run_cmd("drisee -v -p $proc -t $format -d $run_dir -f $infile $d_stats > $d_info", 1);
+    # create drisee table
+    PipelineAWE::run_cmd("drisee -v -p $proc -t $format -d $run_dir -f $infile $d_stats > $d_info", 1);
 
-  # create consensus table
-  my $max_ln = 600;
-  
-  if (exists $value_opts{"max_ln"}) {
-      $max_ln = min ($max_ln, $value_opts{"max_ln"});
-  } else {
-    my @new_stats = `seq_length_stats.py -i $infile -t $format -f`;
-    if (@new_stats) {
-      chomp @new_stats;
-      my $max_line   = first { $_ =~ /^length_max/ } @new_stats;
-      my $new_max_ln = (split(/\t/, $max_line))[1];
-      $max_ln = min ($max_ln, $new_max_ln);
+    # create consensus table
+    my $max_ln = 600;
+    
+    if (exists $value_opts{"max_ln"}) {
+        $max_ln = min ($max_ln, $value_opts{"max_ln"});
     } else {
-      $max_ln = 100;
+        my @new_stats = `seq_length_stats.py -i $infile -t $format -f`;
+        if (@new_stats) {
+            chomp @new_stats;
+            my $max_line   = first { $_ =~ /^length_max/ } @new_stats;
+            my $new_max_ln = (split(/\t/, $max_line))[1];
+            $max_ln = min ($max_ln, $new_max_ln);
+        } else {
+            $max_ln = 100;
+        }
     }
-  }
-  PipelineAWE::run_cmd("consensus.py -v -b $max_ln -t $format -i $infile -o $c_stats");
-  PipelineAWE::run_cmd("touch $a_file");
-  PipelineAWE::run_cmd("touch $a_file.stats");
+    PipelineAWE::run_cmd("consensus.py -v -b $max_ln -t $format -i $infile -o $c_stats");
+    PipelineAWE::run_cmd("touch $a_file");
 } else {
-  PipelineAWE::run_cmd("touch $d_stats");
-  PipelineAWE::run_cmd("touch $d_info");
-  PipelineAWE::run_cmd("touch $c_stats");
-  # create assembly abundance file
-  my $cov_found_count = 0;
-  my $total_reads = 0;
-  open ABUN, ">$a_file" || exit __LINE__;
-  open SEQS, $infile || exit __LINE__;
-  while (my $line = <SEQS>) {
-    chomp $line;
-    if ($line =~ /^>(\S+\_\[cov=(\S+)\]\S*).*$/) {
-      my $seq = $1;
-      my $abun = $2;
-      print ABUN "$seq\t$abun\n";
-      $cov_found_count++;
-      $total_reads++;
-    } elsif ($line =~ /^>(\S+).*$/) {
-      my $seq = $1;
-      print ABUN "$seq\t1\n";
-      $total_reads++;
+    PipelineAWE::run_cmd("touch $d_stats");
+    PipelineAWE::run_cmd("touch $d_info");
+    PipelineAWE::run_cmd("touch $c_stats");
+    # create assembly abundance file
+    my $cov_found_count = 0;
+    my $total_reads = 0;
+    open ABUN, ">$a_file" || exit __LINE__;
+    open SEQS, $infile || exit __LINE__;
+    while (my $line = <SEQS>) {
+        chomp $line;
+        if ($line =~ /^>(\S+\_\[cov=(\S+)\]\S*).*$/) {
+            my $seq = $1;
+            my $abun = $2;
+            print ABUN "$seq\t$abun\n";
+            $cov_found_count++;
+            $total_reads++;
+        } elsif ($line =~ /^>(\S+).*$/) {
+            my $seq = $1;
+            print ABUN "$seq\t1\n";
+            $total_reads++;
+        }
     }
-  }
-  close SEQS;
-  close ABUN;
-  # create assembly abundace summary
-  open OUT, ">$a_file.stats" || exit __LINE__;
-  my $percent = sprintf( "%.2f", ( int ( ( ($cov_found_count / $total_reads) * 10000 ) + 0.5 ) ) / 100 );
-  print OUT "Percentage_of_reads_with_coverage_info:\t$percent\n";
-  close OUT;
+    close SEQS;
+    close ABUN;
+    # create assembly abundace stats
+    my $percent = sprintf( "%.2f", ( int ( ( ($cov_found_count / $total_reads) * 10000 ) + 0.5 ) ) / 100 );
+    $qc_stat->{'percentage_of_reads_with_coverage_info'} = $percent;
 }
 
 # create kmer profile
 foreach my $len (@kmers) {
-  PipelineAWE::run_cmd("kmer-tool -l $len -p $proc -i $infile -t $format -o $out_prefix.kmer.$len.stats -f histo -r -d $run_dir");
+    PipelineAWE::run_cmd("kmer-tool -l $len -p $proc -i $infile -t $format -o $out_prefix.kmer.$len.stats -f histo -r -d $run_dir");
 }
+
+# get summary drisee
+if (-s $d_stats) {
+    my $d_score = `head -2 $d_stats | tail -1 | cut -f8`;
+    chomp $d_score;
+    $qc_stat->{"drisee_score_raw"} = sprintf("%.3f", $d_score);
+}
+
+# process stats into JSON struct
+my $qc = {
+    "drisee"     => get_drisee($d_stats, $qc_stat),
+    "bp_profile" => get_nucleo($c_stats),
+    "kmer"       => {}
+};
+foreach my $len (@kmers) {
+    $qc->{"kmer"}->{$len."_mer"} = get_kmer("$out_prefix.kmer.$len.stats", $len);
+}
+
+# output qc stats
+my $json = JSON->new;
+open(OUTF, ">$output") || exit __LINE__;
+print OUTF json->encode($qc);
+close(OUTF)
 
 exit(0);
 
 sub get_usage {
     return "USAGE: awe_qc.pl -input=<input file> -out_prefix=<output prefix> [-proc=<number of threads, default 8>, -kmers=<kmer list, default 6,15>, -assembled=<0 or 1, default 0>]\noutputs: \${out_prefix}.drisee.stats, \${out_prefix}.drisee.info, \${out_prefix}.consensus.stats, \${out_prefix}.kmer.\$len.stats, \${out_prefix}.assembly.coverage, \${out_prefix}.assembly.coverage.stats\n";
+}
+
+sub get_drisee {
+    my ($dfile, $stats) = @_;
+    
+    my $bp_set = ['A', 'T', 'C', 'G', 'N', 'InDel'];
+    my $drisee = PipelineAWE::file_to_array($dfile);
+    my $ccols  = ['Position'];
+    map { push @$ccols, $_.' match consensus sequence' } @$bp_set;
+    map { push @$ccols, $_.' not match consensus sequence' } @$bp_set;
+    my $data = { summary  => { columns => [@$bp_set, 'Total'], data => undef },
+                 counts   => { columns => $ccols, data => undef },
+                 percents => { columns => ['Position', @$bp_set, 'Total'], data => undef }
+               };
+    unless ($drisee && (@$drisee > 2) && ($drisee->[1][0] eq '#')) {
+        return $data;
+    }
+    for (my $i=0; $i<6; $i++) {
+        $data->{summary}{data}[$i] = $drisee->[1][$i+1] * 1.0;
+    }
+    $data->{summary}{data}[6] = $stats->{drisee_score_raw} ? $stats->{drisee_score_raw} * 1.0 : undef;
+    my $raw = [];
+    my $per = [];
+    foreach my $row (@$drisee) {
+        next if ($row->[0] =~ /\#/);
+	    @$row = map { int($_) } @$row;
+	    push @$raw, $row;
+	    if ($row->[0] > 50) {
+	        my $x = shift @$row;
+	        my $sum = sum @$row;
+	        if ($sum == 0) {
+	            push @$per, [ $x, 0, 0, 0, 0, 0, 0, 0 ];
+	        } else {
+	            my @tmp = map { sprintf("%.2f", 100 * (($_ * 1.0) / $sum)) * 1.0 } @$row;
+	            push @$per, [ $x, @tmp[6..11], sprintf("%.2f", sum(@tmp[6..11])) * 1.0 ];
+            }
+	    }
+    }
+    $data->{counts}{data} = $raw;
+    $data->{percents}{data} = $per;
+    return $data;
+}
+
+sub get_nucleo {
+    my ($nfile) = @_;
+    
+    my $cols = ['Position', 'A', 'T', 'C', 'G', 'N', 'Total'];
+    my $nuc  = PipelineAWE::file_to_array($nfile);
+    my $data = { counts   => { columns => $cols, data => undef },
+                 percents => { columns => [@$cols[0..5]], data => undef }
+               };
+    unless ($nuc && (@$nuc > 2)) {
+        return $data;
+    }
+    my $raw = [];
+    my $per = [];
+    foreach my $row (@$nuc) {
+        next if (($row->[0] eq '#') || (! $row->[6]));
+        @$row = map { int($_) } @$row;
+        push @$raw, [ $row->[0] + 1, $row->[1], $row->[4], $row->[2], $row->[3], $row->[5], $row->[6] ];
+        unless (($row->[0] > 100) && ($row->[6] < 1000)) {
+    	    my $sum = $row->[6];
+    	    if ($sum == 0) {
+	            push @$per, [ $row->[0] + 1, 0, 0, 0, 0, 0 ];
+	        } else {
+    	        my @tmp = map { floor(100 * 100 * (($_ * 1.0) / $sum)) / 100 } @$row;
+    	        push @$per, [ $row->[0] + 1, $tmp[1], $tmp[4], $tmp[2], $tmp[3], $tmp[5] ];
+	        }
+        }
+    }
+    $data->{counts}{data} = $raw;
+    $data->{percents}{data} = $per;
+    return $data;
+}
+
+sub get_kmer {
+    my ($kfile, $num) = @_;
+    
+    my $cols = [ 'count of identical kmers of size N',
+    			 'number of times count occures',
+    	         'product of column 1 and 2',
+    	         'reverse sum of column 2',
+    	         'reverse sum of column 3',
+    		     'ratio of column 5 to total sum column 3 (not reverse)'
+               ];
+    my $kmer = PipelineAWE::file_to_array($kfile);
+    my $data = { columns => $cols, data => undef };
+    unless ($kmer && (@$kmer > 1)) {
+        return $data;
+    }
+    foreach my $row (@$kmer) {
+        @$row = map { $_ * 1.0 } @$row;
+    }
+    $data->{data} = $kmer;
+    return $data;
 }
