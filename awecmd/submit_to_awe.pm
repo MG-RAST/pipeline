20d3f23adbdfb32d4ed4d74c5c9811ecf99abc23
Travis Harrison <guumby@gmail.com>
2014-05-12 16:22:04 -0500

    finish submit script

--- awecmd/submit_to_awe.pm
+++ awecmd/submit_to_awe.pm
@@ -20,6 +20,7 @@ use DBI;
 use JSON;
 use Template;
 use Getopt::Long;
+use File::Basename;
 use LWP::UserAgent;
 use Data::Dumper;
 
@@ -57,11 +58,24 @@ if ($help){
     exit __LINE__;
 }
 
-# replacement variables
+# set obj handles
+my $tt    = Template->new();
+my $agent = LWP::UserAgent->new();
+my $json  = JSON->new;
+$json = $json->utf8();
+$json->max_size(0);
+$json->allow_nonref;
+
+# get default urls
 my $vars = $PipelineAWE_Conf::template_keywords;
-# replace optional
 if ($shock_url) {
-    $vars->{shock_url} = $shock_url
+    $vars->{shock_url} = $shock_url;
+}
+if (! $awe_url) {
+    $awe_url = $PipelineAWE_Conf::awe_url;
+}
+if (! $template) {
+    $template = $PipelineAWE_Conf::template_file;
 }
 
 # get job related info from DB
@@ -71,6 +85,7 @@ unless ($jobj && (scalar(keys %$jobj) > 0) && exists($jobj->{options})) {
     print STDERR get_usage();
     exit __LINE__;
 }
+my $jstat = PipelineAWE::get_job_statistics($job_id);
 my $jattr = PipelineAWE::get_job_attributes($job_id);
 my $jopts = {};
 foreach my $opt (split(/\&/, $jobj->{options})) {
@@ -78,18 +93,116 @@ foreach my $opt (split(/\&/, $jobj->{options})) {
     $jopts->{$k} = $v;
 }
 
-# populate variables
+# build upload attributes
+my $up_attr = {
+    id          => 'mgm'.$jobj->{metagenome_id},
+    job_id      => $job_id,
+    name        => $jobj->{name},
+    created     => $jobj->{created_on},
+    status      => 'private',
+    assembled   => $jattr->{assembled} ? 'yes' : 'no',
+    data_type   => 'sequence',
+    seq_format  => 'bp',
+    file_format => ($jattr->{file_type} && ($jattr->{file_type} == 'fastq')) ? 'fastq' : 'fasta',
+    stage_id    => '050',
+    stage_name  => 'upload',
+    type        => 'metagenome',
+    statistics  => {},
+    sequence_type    => $jobj->{sequence_type} || $jattr->{sequence_type_guess},
+    pipeline_version => $vars->{pipeline_version}
+};
+if ($jobj->{project_id} && $jobj->{project_name}) {
+    $up_attr->{project_id}   = 'mgp'.$jobj->{project_id};
+    $up_attr->{project_name} = $jobj->{project_name};
+}
+foreach my $s (keys %$jstat) {
+    if ($s =~ /(.+)_raw$/) {
+        $up_attr->{statistics}{$1} = $jstat->{$s};
+    }
+}
+
+# upload input to shock
+$HTTP::Request::Common::DYNAMIC_FILE_UPLOAD = 1;
+my $content = {
+    upload     => $input,
+    attributes => [undef, "$input.json", Content => $json->encode($up_attr)];
+};
+my $spost = $agent->post(
+    'http://'.$vars->{shock_url}.'/node',
+    'Authorization', "OAuth $shock_pipeline_token",
+    'Content_Type', 'multipart/form-data',
+    'Content', $content
+);
+my $sres = $json->decode($spost->content);
+
+# populate workflow variables
 $vars->{job_id}         = $job_id;
-$vars->{mg_id}          = 'mgm'.$jobj->{metagenome_id};
-$vars->{mg_name}        = $jobj->{name};
-$vars->{job_date}       = $jobj->{created_on};
-$vars->{project_id}     = 'mgp'.$jobj->{project_id} || '';
-$vars->{project_name}   = $jobj->{project_name} || '';
+$vars->{mg_id}          = $up_attr->{id};
+$vars->{mg_name}        = $up_attr->{name};
+$vars->{job_date}       = $up_attr->{created};
+$vars->{seq_type}       = $up_attr->{sequence_type};
+$vars->{project_id}     = $up_attr->{project_id} || '';
+$vars->{project_name}   = $up_attr->{project_name} || '';
 $vars->{user}           = 'mgu'.$jobj->{owner} || '';
+$vars->{inputfile}      = basename($input);
+$vars->{shock_node}     = $sres->{data}{id};
 $vars->{filter_options} = $jopts->{filter_options} || 'skip';
 $vars->{assembled}      = $jattr->{assembled} || 0;
+$vars->{dereplicate}    = $jopts->{dereplicate} || 1;
+$vars->{bowtie}         = $jopts->{bowtie} || 1;
+$vars->{screen_indexes} = $jopts->{screen_indexes} || 'h_sapiens';
 
+# build bowtie index list
+$vars->{index_download_urls} = "";
+foreach my $idx (split(/,/, $vars->{screen_indexes})) {
+    if (exists $PipelineAWE_Conf::shock_bowtie_indexes->{$idx}) {
+        while (my ($ifile, $iurl) = each %{$PipelineAWE_Conf::shock_bowtie_indexes->{$idx}}) {
+            $vars->{index_download_urls} .= qq(
+                    "$ifile": {
+                        "url": "$iurl"
+                    });
+        }
+    }
+}
+if ($vars->{index_download_urls} eq "") {
+    print STDERR "ERROR: No valid bowtie indexes found in ".$vars->{screen_indexes}."\n";
+    print STDERR get_usage();
+    exit __LINE__;
+}
+
+# replace variables
+my $workflow = $PipelineAWE_Conf::temp_dir/$job_num.".awe_workflow.json";
+$tt->process($template, $vars, $workflow) || die $tt->error()."\n";
+
+# submit to AWE
+my $apost = $agent->post(
+    'http://'.$awe_url.'/job',
+    'Datatoken', $shock_pipeline_token,
+    'Content_Type', 'multipart/form-data',
+    'Content', [ upload => [$workflow] ]
+);
+my $ares = $json->decode($apost->content);
+
+# get info
+my $awe_id  = $ares->{data}{id};
+my $awe_job = $ares->{data}{jid};
+my $state   = $ares->{data}{state};
+
+# add to lookup table
+my $tbl = $PipelineAWE_Conf::awe_mgrast_lookup_table;
+my $dbh = DBI->connect(
+    "DBI:mysql:".$PipelineAWE_Conf::awe_mgrast_lookup_db.";host=".$PipelineAWE_Conf::awe_mgrast_lookup_host,
+    $PipelineAWE_Conf::awe_mgrast_lookup_user,
+    $PipelineAWE_Conf::awe_mgrast_lookup_pass
+    ) || die DBI->errstr."\n";
+my $cmd = "INSERT INTO $tbl (job, awe_id, awe_url, status, submitted, last_update) VALUES ($job_id, '$awe_id', 'http://$awe_url/job/$awe_job', '$state', now(), now())";
+my $sth = $dbh->prepare($str);
+$sth->execute() || die $sth->errstr."\n";
 
 sub get_usage {
     return "USAGE: submit_to_awe.pl -job_id=<job identifier> -input=<input file> [-awe_url=<awe url> -shock_url=<shock url> -template=<template file>]\n";
 }
+
+# enable hash-resolving in the JSON->encode function
+sub TO_JSON { return { %{ shift() } }; }
+
