33840b2879e4dcbc8bd1cd7972efd62130813ffb
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-11-07 09:58:59 -0600

    find m5nr_v1.bdb in working directory

--- awecmd/awe_annotate_sims.pl
+++ awecmd/awe_annotate_sims.pl
@@ -22,6 +22,13 @@ my $ann_file   = "/mnt/awe/data/predata/m5nr_v1.bdb";
 my $aa         = 0;
 my $rna        = 0;
 my $help       = 0;
+
+
+if (-e "m5nr_v1.bdb") {
+	$ann_file = "m5nr_v1.bdb"; # this and path above are temporary hacks
+}
+
+
 my $options    = GetOptions (
         "out_prefix=s" => \$out_prefix,
 		"input=s"      => \$input,
