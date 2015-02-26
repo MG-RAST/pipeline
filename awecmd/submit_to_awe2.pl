4746b976e24faa81337c622c48f0f8c36aa898fb
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-02-26 13:57:23 -0600

    fix

--- awecmd/submit_to_awe2.pl
+++ awecmd/submit_to_awe2.pl
@@ -270,6 +270,7 @@ if (defined($input_node) && $job_id eq "") {
 
 #read jobdb
 if (defined($job_id) && length($job_id) > 0 ) {
+	print "read jobdb...\n"; 
 	($jobj, $jstat, $jattr, $jopts)  = read_jobdb($job_id);
 	
 	foreach my $s (keys %$jstat) {
