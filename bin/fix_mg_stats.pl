2d193cc8d99b22ac38dc7c90616f47748f26dd7d
Travis Harrison <guumby@gmail.com>
2016-04-10 16:50:53 -0500

    fix stats script bugs

--- bin/fix_mg_stats.pl
+++ bin/fix_mg_stats.pl
@@ -134,7 +134,7 @@ sub obj_from_url {
     my $content = undef;
     my $result  = undef;
     my @args    = $key ? ('authorization', "mgrast ".$key) : ();
-    print STDERR "\"authorization: mgrast $auth\" -> ".$url;
+    print STDERR "\"authorization: mgrast $key\" -> ".$url;
     if ($data && ref($data)) {
         push @args, ('Content-Type', 'application/json');
         $result = $agent->post($url, @args, 'Content' => $json->encode($data));
