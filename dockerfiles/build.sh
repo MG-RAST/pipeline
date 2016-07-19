b8277a76031e217bf61521de4c64d47900e806f5
Travis Harrison <guumby@gmail.com>
2016-07-19 12:26:25 -0500

    dockerfiles update

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-export TAG=`date +"%Y%m%d"`
-
-docker rmi mgrast/pipeline-base:latest
-
 set -e
 set -x
 
-docker build --force-rm --no-cache --tag mgrast/pipeline-base:${TAG} mgrast_base
+export TAG=`date +"%Y%m%d"`
 
+# create base
+docker rmi mgrast/pipeline-base:latest
+docker build --force-rm --no-cache --tag mgrast/pipeline-base:${TAG} mgrast_base
 docker tag mgrast/pipeline-base:${TAG} mgrast/pipeline-base:latest
 
+# get ADDs
+# wget -O third-party/blat/superblat <superblat URL>
+# wget -O third-party/search/usearch <usearch URL>
+
+# create children
 cd third-party
 for i in *
     do docker build --force-rm --no-cache --tag mgrast/pipeline-${i}:${TAG} ${i}
