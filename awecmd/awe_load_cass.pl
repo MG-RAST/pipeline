364b858208d88599b7c5eccb6c250a4ee0839b7e
Travis Harrison <guumby@gmail.com>
2016-10-28 11:02:12 -0500

    usage added

--- awecmd/awe_load_cass.pl
+++ awecmd/awe_load_cass.pl
@@ -117,3 +117,6 @@ sub toFloat {
     return $x * 1.0;
 }
 
+sub get_usage {
+    return "USAGE: awe_load_cass.pl -job=<job identifier> -md5=<md5 abundance file> -lca=<lca abundance file> -api_url <mgrast api url> -ann_ver <m5nr annotation version #>\n";
+}
