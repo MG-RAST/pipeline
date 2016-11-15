89893bac92fed525dde42bcb597f306e4471a497
Travis Harrison <guumby@gmail.com>
2016-11-15 11:35:46 -0600

    update completion email

--- awecmd/awe_notify.pl
+++ awecmd/awe_notify.pl
@@ -9,7 +9,7 @@ use Getopt::Long;
 umask 000;
 
 # globals
-my $mg_link = 'http://metagenomics.anl.gov/linkin.cgi?metagenome=';
+my $site = 'http://metagenomics.anl.gov';
 my $api_url = "";
 my $user = "";
 my $mgid = "";
@@ -18,7 +18,7 @@ my $help = 0;
 my $options = GetOptions (
     "api_url=s" => \$api_url,
     "user=s" => \$user,
-    "mgid=s" => \$mgid,
+    "mgid:s" => \$mgid,
     "help!"  => \$help
 );
 
@@ -28,25 +28,18 @@ if ($help){
 }elsif (length($user)==0){
     PipelineAWE::logger('error', "user ID is required");
     exit 1;
-}elsif (length($mgid)==0){
-    PipelineAWE::logger('error', "MG-RAST ID is required");
-    exit 1;
 }
 
 # get api variable
 my $api_key = $ENV{'MGRAST_WEBKEY'} || undef;
 
 # get info
-my $user_attr = PipelineAWE::get_userattr();
+my $job_info  = PipelineAWE::get_userattr();
 my $user_info = PipelineAWE::get_user_info($user, $api_url, $api_key);
 
 # email owner on completion
-my $link = $mgid;
-$link =~ s/^mgm(.*)/$1/;
-$link = $mg_link.$link;
-my $name = $user_attr->{name} ? " for ".$user_attr->{name} : "";
-my $body_txt = "The annotation job that you submitted$name has completed.\n\n".
-               "You can view you job here:\n$link\n\n".
+my $body_txt = "Your submitted annotation job ".$job_info->{name}." belonging to study ".$job_info->{project_name}." has completed.\n\n".
+               "Log in to MG-RAST ($site) to view your results. Your completed data is available through the 'My Studies' section on your 'My Data' page.\n".
                "PLEASE NOTE: Your data has NOT been made public and ONLY you can currently view the data and results.\n".
 		"If you wish to publicly share the link to your results, you will need to make the data public yourself. This is needed even if you selected that the data is going to be made public immediately after completion.\n".
                'This is an automated message. Please contact mg-rast@mcs.anl.gov if you have any questions or concerns.';
@@ -55,5 +48,5 @@ PipelineAWE::send_mail($body_txt, "MG-RAST Job Completed", $user_info);
 exit 0;
 
 sub get_usage {
-    return "USAGE: awe_notify.pl -user=<user identifier> -mgid=<mgrast identifier>\n";
+    return "USAGE: awe_notify.pl -user=<user identifier>\n";
 }
