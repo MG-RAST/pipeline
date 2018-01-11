0f0d97f3f37149246ae8a6e121aa398166d26db7
Travis Harrison <guumby@gmail.com>
2018-01-11 13:33:14 -0600

    updating sims_annotate

--- bin/sims_annotate.pl
+++ bin/sims_annotate.pl
@@ -10,18 +10,14 @@ use Data::Dumper;
 use Getopt::Long;
 
 my $verbose   = 0;
+my $format    = '';
 my $in_file   = '';
 my $in_scg    = '';
 my $filt_file = '';
 my $exp_file  = '';
-my $ont_file  = '';
-my $rna_file  = '';
 my $lca_file  = '';
 my $ann_file  = '';
-my $get_rna   = 0;
 my $get_prot  = 0;
-my $get_ont   = 0;
-my $get_lca   = 0;
 my $frag_num  = 5000;
 my $usage     = qq($0
 Input:
@@ -29,63 +25,48 @@ Input:
     2. scg: md5 single copy gene file in json format
 Output: top hit for each query per source (protein or rna formats)
     1. filtered sims: same format as m8
-    2. rna expanded sims: see below
-    3. lca expanded (rna): see below
-  OR
-    1. filtered sims: same format as m8
     2. expanded sims: see below
-    3. ontology sims: see below
-    4. lca expanded (protein): see below
+    3. lca expanded: see below
 
 m8:       subject|fragment, query|md5, identity, length, mismatch, gaps, q_start, q_end, s_start, s_end, evalue, bit_score
-expanded: md5|query, fragment|subject, identity, length, evalue, function, organism, source
-rna:      md5|query, fragment|subject, identity, length, evalue, function, organism, source, is_rna
-ontology: md5|query, fragment|subject, identity, length, evalue, function, ontology, source
+expanded: md5|query, fragment|subject, identity, length, evalue, function, organism, source, is_rna
 LCA:      md5|query list, fragment|subject, identity list, length list, evalue list, lca string, depth of lca (1-8)
 
-  --in_sim       file name      Required. Name of input sim file
+  --format       protein|rna    Required. Type of sequences data in input file.
+  --in_sim       file name      Required. Name of input sim file.
   --in_scg       file name      Optional. Name of input md5 single copy gene file.
   --out_filter   file name      Required. Name of filtered sim file.
-  --out_expand   file name      Name of output expanded protein sim file (protein mode only).
-  --out_ontology file name      Name of output expanded ontology sim file (protein mode only).
-  --out_rna      file name      Name of output expanded rna sim file (rna mode only).
+  --out_expand   file name      Name of output expanded sim file (protein mode only).
   --out_lca      file name      Name of output expanded LCA file (protein and rna mode).
   --ann_file     file name      Name of berkley DB formatted m5nr annotations.
-  --frag_num     int            Optional. Number of fragment chunks to load in memory at once before processing. Default is '$frag_num'
+  --frag_num     int            Optional. Number of fragment chunks to load in memory at once before processing. Default is '$frag_num'.
   --verbose                     Optional. Verbose output.
 
 );
 if ( (@ARGV > 0) && ($ARGV[0] =~ /-h/) ) { print STDERR $usage; exit 1; }
-if ( ! GetOptions( "verbose!"       => \$verbose,
-		           "in_sim=s"       => \$in_file,
-                   "in_scg=s"       => \$in_scg,
-		           "out_filter=s"   => \$filt_file,
-		           "out_expand:s"   => \$exp_file,
-		           "out_ontology:s" => \$ont_file,
-		           "out_rna:s"      => \$rna_file,
-		           "out_lca:s"      => \$lca_file,
-		           "ann_file=s"     => \$ann_file,
-		           "frag_num:i"     => \$frag_num
+if ( ! GetOptions( "verbose!"     => \$verbose,
+                   "format=s"     => \$format,
+		           "in_sim=s"     => \$in_file,
+                   "in_scg=s"     => \$in_scg,
+		           "out_filter=s" => \$filt_file,
+		           "out_expand=s" => \$exp_file,
+		           "out_lca=s"    => \$lca_file,
+		           "ann_file=s"   => \$ann_file,
+		           "frag_num:i"   => \$frag_num
                  ) )
   { print STDERR $usage; exit 1; }
 
 unless ($in_file && (-s $in_file) && $filt_file) {
   print STDERR $usage . "Missing input and/or filter files.\n"; exit 1;
 }
-if ($rna_file) {
-  print "Running in rna sim mode.\n" if ($verbose);
-  $get_rna = 1;
-} elsif ($exp_file) {
-  print "Running in protein sim mode.\n" if ($verbose);
-  $get_prot = 1;
-}
-if ($ont_file && (! $rna_file)) {
-  print "Running in ontology mode (not available in rna mode).\n" if ($verbose);
-  $get_ont = 1;
-}
-if ($lca_file) {
-  print "Running in lca mode.\n" if ($verbose);
-  $get_lca = 1;
+
+if ($format eq 'protein') {
+    $get_prot = 1;
+    print "Running in protein sim mode.\n" if ($verbose);
+} elsif ($format eq 'rna') {
+    print "Running in rna sim mode.\n" if ($verbose);
+} else {
+    print STDERR $usage . "Invalid format value.\n"; exit 1;
 }
 
 # get scg hash
@@ -106,7 +87,7 @@ my $data  = {};
 my $count = 0;
 my $frags = 0;
 my $curr  = '';
-my ($filt_fh, $exp_fh, $ont_fh, $rna_fh, $lca_fh, $filt_t, $exp_t, $ont_t, $rna_t, $lca_t);
+my ($filt_fh, $exp_fh, $lca_fh, $filt_t, $exp_t, $lca_t);
 
 print "Loading ach source data for mapping ... " if ($verbose);
 my $src_map = $json->decode($m5nr{'source'});
@@ -118,18 +99,8 @@ print "Done\n" if ($verbose);
 print "Parsing file $in_file in $frag_num fragment size chunks ... " if ($verbose);
 open(INFILE, "<$in_file") or die "Can't open file $in_file!\n";
 open($filt_fh, ">$filt_file") or die "Can't open file $filt_file!\n";
-if ($get_rna) {
-  open($rna_fh, ">$rna_file") or die "Can't open file $rna_file!\n";
-}
-if ($get_prot) {
-  open($exp_fh, ">$exp_file") or die "Can't open file $exp_file!\n";
-}
-if ($get_ont) {
-  open($ont_fh, ">$ont_file") or die "Can't open file $ont_file!\n";
-}
-if ($get_lca) {
-  open($lca_fh, ">$lca_file") or die "Can't open file $lca_file!\n";
-}
+open($exp_fh, ">$exp_file") or die "Can't open file $exp_file!\n";
+open($lca_fh, ">$lca_file") or die "Can't open file $lca_file!\n";
 
 while (my $line = <INFILE>) {
   chomp $line;
@@ -144,11 +115,9 @@ while (my $line = <INFILE>) {
   if ($curr ne $frag) {
     if ($frags >= $frag_num) {
       #print STDERR "." if ($verbose);
-      ($filt_t, $exp_t, $ont_t, $rna_t, $lca_t) = &get_top_hits($src_map, $data);
+      ($filt_t, $exp_t, $lca_t) = &get_top_hits($src_map, $data, $get_prot);
       print $filt_fh $filt_t if ($filt_t);
       print $exp_fh $exp_t   if ($exp_t);
-      print $ont_fh $ont_t   if ($ont_t);
-      print $rna_fh $rna_t   if ($rna_t);
       print $lca_fh $lca_t   if ($lca_t);
       $data = {};
       $frags = 0;
@@ -167,38 +136,24 @@ while (my $line = <INFILE>) {
 close(INFILE);
 
 if (scalar(keys %$data) > 0) {
-  ($filt_t, $exp_t, $ont_t, $rna_t, $lca_t) = &get_top_hits($src_map, $data);
+  ($filt_t, $exp_t, $lca_t) = &get_top_hits($src_map, $data, $get_prot);
   print $filt_fh $filt_t if ($filt_t);
   print $exp_fh $exp_t   if ($exp_t);
-  print $ont_fh $ont_t   if ($ont_t);
-  print $rna_fh $rna_t   if ($rna_t);
   print $lca_fh $lca_t   if ($lca_t);
 }
 
 close($filt_fh);
-if ($get_rna) {
-  close($rna_fh)
-}
-if ($get_prot) {
-  close($exp_fh);
-}
-if ($get_ont) {
-  close($ont_fh);
-}
-if ($get_lca) {
-  close ($lca_fh);
-}
+close($exp_fh);
+close ($lca_fh);
 
 print "Done - $count fragments parsed\n" if ($verbose);
 exit 0;
 
 sub get_top_hits {
-  my ($src_map, $data) = @_;
+  my ($src_map, $data, $get_prot) = @_;
   
   my $filter_text   = '';
-  my $protein_text  = '';
-  my $ontology_text = '';
-  my $rna_text      = '';
+  my $expand_text   = '';
   my $lca_text      = '';
   my $data_min_md5  = {};
   my $total_min_md5 = {};
@@ -213,10 +168,10 @@ sub get_top_hits {
     foreach my $score (keys %{$data->{$frag}}) {
       foreach my $md5 (keys %{$data->{$frag}{$score}}) {
 	    $total_md5s->{$md5} = 1;
-	    if ($get_lca) {
-	      my $exp = &get_exp( $data->{$frag}{$score}{$md5}[8] );
-	      if (defined $exp) { push @{ $frag_md5s->{$frag} }, [ $md5, $exp, $data->{$frag}{$score}{$md5} ]; }
-	    }
+	    my $exp = &get_exp( $data->{$frag}{$score}{$md5}[8] );
+	    if (defined $exp) {
+          push @{ $frag_md5s->{$frag} }, [ $md5, $exp, $data->{$frag}{$score}{$md5} ];
+        }
       }
     }
   }
@@ -225,7 +180,7 @@ sub get_top_hits {
   # get data for md5s from memcache
   # ach: md5 => source => function => [organism | ontology_id]
   # lca: md5 => [lca]
-  my ($md5_ach, $md5_lca) = &get_md5_data([keys %$total_md5s], $get_lca);
+  my ($md5_ach, $md5_lca) = &get_md5_data([keys %$total_md5s]);
 
   # get sources per frag: frag => {source}
   foreach my $frag (keys %$data) {
@@ -237,22 +192,20 @@ sub get_top_hits {
   }
 
   # get lca per frag: frag => { md5s => [md5], sims => [sim], lca => [lca] }
-  if ($get_lca) {
-    foreach my $frag (keys %$frag_md5s) {
-      my %md5s_lca  = map { $_->[0], $_->[2] } grep { $_->[1] == 0 } @{$frag_md5s->{$frag}};
-      my @sort_md5s = sort { $a->[1] <=> $b->[1] } grep { $_->[1] < 0 } @{$frag_md5s->{$frag}};
+  foreach my $frag (keys %$frag_md5s) {
+    my %md5s_lca  = map { $_->[0], $_->[2] } grep { $_->[1] == 0 } @{$frag_md5s->{$frag}};
+    my @sort_md5s = sort { $a->[1] <=> $b->[1] } grep { $_->[1] < 0 } @{$frag_md5s->{$frag}};
 
-      if (@sort_md5s > 0) {   
-	    my $cutoff = $sort_md5s[0][1] - int($sort_md5s[0][1] * 0.2);
-	    foreach my $set (@sort_md5s) {
-	      if ($set->[1] <= $cutoff) { $md5s_lca{ $set->[0] } = $set->[2]; }
-	    }
-      }
-      if (scalar(keys %md5s_lca) > 0) {
-	    my @lca = &get_lca([keys %md5s_lca], $md5_lca);
-	    if (@lca == 9) {
-	      $frag_lca->{$frag} = {md5s => [sort keys %md5s_lca], sims => [sort values %md5s_lca], lca => \@lca};
-	    }
+    if (@sort_md5s > 0) {   
+	  my $cutoff = $sort_md5s[0][1] - int($sort_md5s[0][1] * 0.2);
+	  foreach my $set (@sort_md5s) {
+        if ($set->[1] <= $cutoff) { $md5s_lca{ $set->[0] } = $set->[2]; }
+	  }
+    }
+    if (scalar(keys %md5s_lca) > 0) {
+	  my @lca = &get_lca([keys %md5s_lca], $md5_lca);
+	  if (@lca == 9) {
+	    $frag_lca->{$frag} = {md5s => [sort keys %md5s_lca], sims => [sort values %md5s_lca], lca => \@lca};
       }
     }
   }
@@ -273,7 +226,7 @@ sub get_top_hits {
 
   # output expanded info
   foreach my $frag (sort keys %$data) {
-    if ($get_lca && exists($frag_lca->{$frag})) {
+    if (exists($frag_lca->{$frag})) {
       my $level = pop @{$frag_lca->{$frag}{lca}};
       $lca_text .= join("\t", ( join(";", grep {exists $md5_lca->{$_}} @{$frag_lca->{$frag}{md5s}}), # md5s
 				                $frag,
@@ -304,20 +257,12 @@ sub get_top_hits {
 	      foreach my $func ( keys %{$md5_ach->{$md5}{$src}} ) {
 	        foreach my $otype ( keys %{$md5_ach->{$md5}{$src}{$func}} ) {
 	          foreach my $other ( @{$md5_ach->{$md5}{$src}{$func}{$otype}} ) {
-	            my $expand_text = join("\t", ($md5, $frag, $sim->[0], $sim->[1], $sim->[8], ($func || ""), $other, $src));
-	            my $expand_wout_fo_text = join("\t", ($md5, $frag, $sim->[0], $sim->[1], $sim->[8], "", "", $src));
-	            if (($otype eq 'organism') && ($stype eq 'rna') && $get_rna) {
-		          $rna_text .= $expand_text . "\t1\n";
+	            my $expand_line = join("\t", ($md5, $frag, $sim->[0], $sim->[1], $sim->[8], ($func || ""), $other, $src));
+	            if (($otype eq 'organism') && ($stype eq 'rna') && (! $get_prot)) {
+		          $expand_text .= $expand_line . "\t1\n";
 	            }
 	            elsif (($otype eq 'organism') && ($stype eq 'protein') && $get_prot) {
-		          $protein_text .= $expand_text . "\n";
-	            }
-	            elsif (($otype eq 'ontology') && ($stype eq 'ontology') && $get_ont) {
-	              if (int($other) > 71073) { next; }
-		          $ontology_text .= $expand_text . "\n";
-		          if ($get_prot) {
-		            $protein_text .= $expand_wout_fo_text . "\n";
-	              }
+		          $expand_text .= $expand_line . "\n";
 	            }
 	          }
 	        }
@@ -326,11 +271,11 @@ sub get_top_hits {
       }
     }
   }
-  return ($filter_text, $protein_text, $ontology_text, $rna_text, $lca_text);
+  return ($filter_text, $expand_text, $lca_text);
 }
 
 sub get_md5_data {
-    my ($md5s, $get_lca) = @_;
+    my ($md5s) = @_;
 
     my $ann   = {};
     my $lca   = {};
@@ -341,7 +286,7 @@ sub get_md5_data {
         }
     }
     foreach my $m (keys %data) {
-        if (exists($data{$m}{lca}) && $get_lca) {
+        if (exists($data{$m}{lca})) {
             $lca->{$m} = $data{$m}{lca};
         }
         if (exists $data{$m}{ann}) {
