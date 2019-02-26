e838f02402ae88e5284c11c4ddc80a978cf9e329
Andreas Wilke <wilke@anl.gov>
2019-02-26 11:55:54 -0600

    Changed path to download script

--- Setup/check-and-load-docker-volume.sh
+++ Setup/check-and-load-docker-volume.sh
@@ -4,7 +4,7 @@ volume=`docker volume ls | grep pipeline-pre-data`
 if [[ "$volume" == "" ]] 
 then
     echo Can not find docker volume pipeline-pre-data, creating volume
-    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:testing  /pipeline/CWL/Inputs/DBs/getpredata.sh /DBs/
+    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:testing  /pipeline/Setup/getpredata.sh /DBs/
 else
     echo Found volume pipeline-pre-data, using it
 fi    
