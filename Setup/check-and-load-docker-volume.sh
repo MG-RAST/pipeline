7d3d8b0e7c0b12192f7643fd2496250b0c8f3f2e
Andreas Wilke <wilke@anl.gov>
2019-02-26 11:51:23 -0600

    Added scripts for setting up testing environment

---
+++ Setup/check-and-load-docker-volume.sh
@@ -0,0 +1,9 @@
+echo Checking for database volume
+volume=`docker volume ls | grep pipeline-pre-data`
+if [[ "$volume" == "" ]] 
+then
+    echo Can not find docker volume pipeline-pre-data, creating volume
+    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:testing  /pipeline/CWL/Inputs/DBs/getpredata.sh /DBs/
+else
+    echo Found volume pipeline-pre-data, using it
+fi    
