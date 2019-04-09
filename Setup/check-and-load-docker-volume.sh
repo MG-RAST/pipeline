6a8727124baf77416ca797982fd4e0689c2a593a
Andreas Wilke <wilke@anl.gov>
2019-04-09 14:52:35 -0500

    Changed image name

--- Setup/check-and-load-docker-volume.sh
+++ Setup/check-and-load-docker-volume.sh
@@ -4,9 +4,9 @@ volume=`docker volume ls | grep pipeline-pre-data`
 if [[ "$volume" == "" ]] 
 then
     echo Can not find docker volume pipeline-pre-data, creating volume
-    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:test  /pipeline/Setup/getpredata.sh /DBs/
+    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:testing  /pipeline/Setup/getpredata.sh /DBs/
 else
     echo Found volume pipeline-pre-data, using it
     # getpredata.sh changed - tests if data exists
-    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:test  /pipeline/Setup/getpredata.sh /DBs/
+    docker run -t -v pipeline-pre-data:/DBs -v `pwd`:/pipeline mgrast/pipeline:testing  /pipeline/Setup/getpredata.sh /DBs/
 fi    
